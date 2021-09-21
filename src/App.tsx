3fdd66ab3ce053acfc2799eab1b7dccf2b18a11e
Sara Pansarini <sara.pansarini@gmail.com>
2021-09-21 20:48:09 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button text="Botão 1" />
+      <Button text="Botão 2" />
+      <Button text="Botão 3" />
+      <Button text="Botão 4" />
+      <Button text="Botão 5" />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
